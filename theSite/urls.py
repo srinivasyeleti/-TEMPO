d854e57d91ac567f17d5b53124f8eea025593822
Srinivas Yeleti <srinivasyeleti84@gmail.com>
2022-07-06 16:11:51 +0530

    complete project

---
+++ theSite/urls.py
@@ -0,0 +1,124 @@
+"""theSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from personal.views import (
+    home_page_view,
+    home_body_view,
+    about_page_view,
+    remove_building_view,
+    wrong_about_page_view,
+    mainHome_page_view,
+
+    weather_view,
+    weather_view_2,
+
+    resident_detail_view,
+    building_detail_view,
+    add_building_view,
+
+    remove_resident_view,
+    remove_building_view,
+)
+
+from account.views import (
+    UserRegistration_view,
+    logout_view,
+    login_view,
+    AccountUpdate_view,
+    admin_page_view,
+
+    all_users_view,
+    all_buildings_view,
+)
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+# import for password actions,
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # myAdmin,
+    # path('custAdmin/', admin_page_view, name = 'admin_page'),
+    path('admin/', admin_page_view, name = 'admin_page'),
+
+    path('', home_page_view, name = "home"),
+
+    path('', home_page_view, name = "home_body"),
+
+    path('aboutus/', about_page_view, name = "about us"),
+    # path('aboutus/', wrong_about_page_view, name = "about us"),
+
+    path('register/', UserRegistration_view, name = "register"),
+
+    path("logout/", logout_view, name = "logout"),
+
+    path("mainHome/", mainHome_page_view, name = "mainHome"),
+
+    path('login/', login_view, name = "login"),
+
+    path('profile/', AccountUpdate_view, name = "My Profile"),
+
+
+
+
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    path('mainHome/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
+        name='password_change_done'),
+
+    path('mainHome/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), 
+        name='password_change'),
+
+    path('mainHome/password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('mainHome/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path('mainHome/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    # path('mainHome/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), 
+    #     name='password_reset'),
+    
+    path('mainHome/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+     name='password_reset_complete'),
+
+    path('weather_info', weather_view, name = "weather_info"),
+
+    path('weather_info_2', weather_view_2, name = "weather_info_2"),
+
+    path('mainHome/our_users', all_users_view, name = "all_users"),
+
+    path('mainHome/our_buildings', all_buildings_view, name = "all_buildings"),
+
+
+    path('resident_details/<str:username>/', resident_detail_view, name='resident_details'),
+    path('building_details/<str:building_id>/', building_detail_view, name='building_details'),
+    path('add_building/', add_building_view, name='add_building'),
+    
+    path('remove_resident/<str:username>/', remove_resident_view, name='remove_resident'),
+    path('remove_building/<str:building_id>/', remove_building_view, name='remove_building'),
+]
+
+
+if settings.DEBUG : # if we are in developement env,
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
